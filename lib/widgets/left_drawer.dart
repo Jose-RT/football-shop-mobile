ac926eca4b002b00a6e2afd6b770366bc2686421
JRT <joseryutoari@gmail.com>
2025-11-19 11:15:53 +0700

    fix left_drawer import

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,62 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:football_shop_mobile/screens/menu.dart';
 import 'package:football_shop_mobile/screens/product_entry_list.dart';
-import 'package:football_shop_mobile/screens/product_form_page.dart';
+import 'package:football_shop_mobile/screens/product_form.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Drawer(
-            child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                    const DrawerHeader(
-                        decoration: BoxDecoration(color: Colors.blue),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                            Text('Football Shop', style: TextStyle(fontSize: 24, color: Colors.white)),
-                            SizedBox(height: 8),
-                            Text('Toko merchandise sepak bola', style: TextStyle(color: Colors.white)),
-                            ],
-                        ),
-                    ),
-                    ListTile(
-                        leading: const Icon(Icons.home),
-                        title: const Text('Halaman Utama'),
-                        onTap: () {
-                            Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(builder: (context) => const HomePage()),
-                            (route) => false,
-                            );
-                        },
-                    ),
-                    ListTile(
-                        leading: const Icon(Icons.add),
-                        title: const Text('Tambah Produk'),
-                        onTap: () {
-                            Navigator.pop(context); // close drawer
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => const ProductFormPage()),
-                            );
-                        },
-                    ),
-                    ListTile(
-                        leading: const Icon(Icons.add_reaction_rounded),
-                        title: const Text('Product List'),
-                        onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => const ProductEntryList()),
-                            );
-                        },
-                    ),
-                ],
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Football Shop',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Toko merchandise sepak bola',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
             ),
-        );
-    }
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Halaman Utama'),
+            onTap: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const HomePage()),
+                (route) => false,
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.add),
+            title: const Text('Tambah Produk'),
+            onTap: () {
+              Navigator.pop(context); // close drawer
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ProductFormPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('Product List'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ProductEntryList(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
